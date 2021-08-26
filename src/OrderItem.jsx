43f3f886d14e23a27f4e62ff3afcc3d198f65b5f
TXHENG <TXHENG@users.noreply.github.com>
2021-08-26 11:34:10 +0800

    fix

--- src/OrderItem.jsx
+++ src/OrderItem.jsx
@@ -25,7 +25,7 @@ const OrderItem = (props) => {
 			<div className="w-2/3 p-1 inline-block">
 				<h1 className="text-red-500 text-md font-bold">{v.name}</h1>
 				<span className="py-1 px-2 rounded-3xl bg-yellow-400 text-white text-xs">
-					<img src={`/imgs/${v.type}.svg`} alt="type" width="15" height="15"/>
+					<img src={`/HCI-Prototype/build/imgs/${v.type}.svg`} alt="type" width="15" height="15"/>
 					<span className="ml-1 text-xs">{v.type}</span>
 				</span>
 				<p className="mt-1 bg-red-300 p-1 text-sm text-white rounded-lg">RM {v.price}.00</p>
